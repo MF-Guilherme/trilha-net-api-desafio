76052357df924276d348258f56bef416e8c4717a
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-11-25 11:45:27 -0300

    Configurações e migrations realizadas

--- Program.cs
+++ Program.cs
@@ -4,9 +4,12 @@ using TrilhaApiDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Adiciona configuração de variáveis de ambiente
+builder.Configuration.AddEnvironmentVariables();
+
 // Add services to the container.
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoDesafio")));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
